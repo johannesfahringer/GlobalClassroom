e904987daf001db8365a0f46b965cfd03e54d83e
johannesfahringer <j.fahringer@hotmail.com>
2018-10-30 15:55:34 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Jojo on 28.10.2018.
+ */
+
+
+
+
+$(document).ready(function(){
+    $('.navbar-fostrap').click(function(){
+        $('.navbar-fostrap').toggleClass('visible');
+        $('body').toggleClass('');
+    });
+});
